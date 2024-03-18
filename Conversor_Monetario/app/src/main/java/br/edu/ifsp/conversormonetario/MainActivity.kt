bc7eacc02d459aa76e186935bbf1ec9f2b1341d0
Matheus-OD074 <matheus.santoslopes45@gmail.com>
2024-03-18 10:30:18 -0300

    Conversor Monetario: v1.0

--- Conversor_Monetario/app/src/main/java/br/edu/ifsp/conversormonetario/MainActivity.kt
+++ Conversor_Monetario/app/src/main/java/br/edu/ifsp/conversormonetario/MainActivity.kt
@@ -1,15 +1,63 @@
 package br.edu.ifsp.conversormonetario
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import java.lang.NumberFormatException
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnClickListener {
+
+    private val DOLLAR_VALUE = 4.99
+
+    private lateinit var inputEditText: EditText
+    private lateinit var convertToDollarButton: Button
+    private lateinit var outputTextView : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        findByID()
+        configClickListener()
+
     }
+
+    override fun onClick(v: View?) {
+
+        when(v){
+            convertToDollarButton -> convertToDollar()
+        }
+
+    }
+
+    private fun findByID (){
+
+        inputEditText = findViewById(R.id.editText_valor)
+        convertToDollarButton = findViewById(R.id.button_convertToDollar)
+        outputTextView = findViewById(R.id.textView_result)
+
+    }
+
+    private fun configClickListener(){
+        convertToDollarButton.setOnClickListener(this)
+    }
+
+    private fun convertToDollar(){
+
+        var value = try{
+            inputEditText.text.toString().toDouble()
+        }catch (e: NumberFormatException){
+            0.0
+        }
+
+        value = value * DOLLAR_VALUE
+
+        outputTextView.text= "US $value"
+
+    }
+
 }
